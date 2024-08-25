e73a2418c49239fc48033c9025c6507a42c60ba9
AvishkaNavindu <navinduavishka2002dfuze@gmail.com>
2024-08-25 16:27:40 +0530

    pushing to avishka branch

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,37 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/home/Header'; 
 import HeroSection from './Components/home/HeroSection';
 
-
-import F_Home from './Components/Footwear & Accessories/F_Home';
-import F_MensCasualAndFormal from './Components/Footwear & Accessories/F_Men\'sCasualAndFormal';
-import F_WomensCasualAndFormal from './Components/Footwear & Accessories/F_Women\'sCasualAndFormal';
-import A_Home from './Components/Footwear & Accessories/A_Home';
-
-
-
 import Footer from './Components/home/Footer';
-
-import Sidebar from './Components/salon/Sidebar';
-import Footer from './Components/home/Footer';
-
-
-//salon
-import AppointmentForm from './Components/salon/AppointmentForm';
-import Salonhome from './Components/salon/Salonhome';
-import ServiceList from './Components/salon/ServiceList';
-import MyAppointmentForm from './Components/salon/MyAppointmentForm';
 
  
-
-
-
-
-
-
-
-
-//review
-import ReviewForm from './Components/Review/ReviewForm';
 
 
 
@@ -46,30 +18,6 @@ function App() {
       <Routes>
 
         <Route path="/" element={<HeroSection />} />
-        <Route path="/salon" element={<><Sidebar /><Salonhome /></>} />
-        <Route path="/ServiceList" element={<><Sidebar /><ServiceList /></>} />
-        <Route path="/AppointmentForm" element={<><Sidebar /><AppointmentForm /></>} />
-        <Route path="/MyAppointmentForm" element={<><Sidebar /><MyAppointmentForm /></>} />
-
-      
-
-      <Route path="/" element={<HeroSection/>} />
-      <Route path="/F_Home" element={<F_Home/>} />
-      <Route path="/F_MensCasualAndFormal" element={<F_MensCasualAndFormal/>} />
-      <Route path="F_WomensCasualAndFormal" element={<F_WomensCasualAndFormal/>} />
-      <Route path="/A_Home" element={<A_Home/>} />
-
-
-
-
-      
-
-
-
-
-      <Route path="/" element={<ReviewForm />} />
-
-      
       
 
       </Routes>
